4b25bd6426c6bb17bb66506648a7a53c3cd445c7
Kailee Gray <kaileeagray@gmail.com>
2019-05-06 16:49:43 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -23,7 +23,7 @@ function init() {
 
       if (index === codes.length) {
         console.log("done!");
-        document.window.alert("Hurray!");
+        document.alert("Hurray!");
         index = 0;
       }
     } else {
